6248fd19eb2b22d318796f9af17839f8bf259f8a
Aakash Mishra <akashmi4082@gmail.com>
2026-03-17 22:15:58 +0530

    first array

---
+++ 01_Array/largest-element.py
@@ -0,0 +1,19 @@
+"""
+Largest Element
+
+Given an array of integers nums, return the value of the largest element in the array
+"""
+
+class Solution:
+    def largestElement(self, nums):
+        largest = nums[0]
+        for i in range(1, len(nums)):
+            if nums[i] > largest:
+                largest = nums[i]
+        return largest
+    
+nums = list(map(int, input().split()))
+obj = Solution()
+result = obj.largestElement(nums)
+print("Largest element is:", result)
+ 
