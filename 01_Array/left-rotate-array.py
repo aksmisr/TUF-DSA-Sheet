3afa35e5e1a7d6e3b77ccb52cfae33146c1f9fc1
Aakash Mishra <akashmi4082@gmail.com>
2026-03-21 22:54:03 +0530

    Add new qsn

--- 01_Array/left-rotate-array.py
+++ 01_Array/left-rotate-array.py
@@ -8,3 +8,11 @@ Given an integer array nums, rotate the array to the left by one.
 Note: There is no need to return anything, just modify the given array.
 
 """
+
+def rotate(nums):
+    first = nums[0]         
+    
+    for i in range(len(nums) - 1):
+        nums[i] = nums[i + 1]  
+    
+    nums[-1] = first        
