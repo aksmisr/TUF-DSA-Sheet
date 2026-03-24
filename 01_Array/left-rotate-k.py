5e18b7e6aefe1b028c17375b585b1ca15f2d03ef
Aakash Mishra <akashmi4082@gmail.com>
2026-03-24 22:39:36 +0530

    add k passw

--- 01_Array/left-rotate-k.py
+++ 01_Array/left-rotate-k.py
@@ -6,4 +6,17 @@ Given an integer array nums, rotate the array to the left by one.
 Note: There is no need to return anything, just modify the given array.
 
 """
-solution
+
+def solve(arr, n):
+    temp = [0] * n 
+
+ 
+    for i in range(1, n):
+        temp[i - 1] = arr[i]
+    temp[n - 1] = arr[0]  
+  
+    for num in temp:
+        print(num, end=" ") 
+    print()
+
+
