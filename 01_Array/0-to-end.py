01c8ee0b3520d1aca6305fb27f39c9459f06f24a
Aakash Mishra <akashmi4082@gmail.com>
2026-03-27 00:00:22 +0530

    new qsn

---
+++ 01_Array/0-to-end.py
@@ -0,0 +1,10 @@
+"""
+Move Zeros to End
+
+Given an integer array nums, move all the 0's to the end of the array. The relative order of the other elements must remain the same.
+
+
+
+This must be done in place, without making a copy of the array.
+
+"""
